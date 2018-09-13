561188f3f574389ea61f0903d4b4438fa5706f33
Glynja <ignoranceuk@gmail.com>
2018-09-13 14:58:39 +0100

    changed marker, added images for different markers

--- public/javascript/marker.js
+++ public/javascript/marker.js
@@ -15,9 +15,19 @@ $.ajax({
 			maxWidth: '5000',
 			className: 'custom'
 		};
+
+		var greenIcon = L.icon({
+    iconUrl: 'images/farm_marker_green.png',
+
+    iconSize:     [60, 60], // size of the icon
+    iconAnchor:   [30, 60], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+		});
+
 		var marker = L.marker([56.4981776, -3.0744827], {
 			elevation: 0,
-			title: 'Farm'
+			title: 'Farm',
+			icon: greenIcon
 		}).addTo(map);
 		marker.bindPopup(customPopup, customOptions).openPopup();
 	}
