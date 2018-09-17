db41743d5342d5049d961819e80ba36a7f010c5b
jsbennett <bnnttjy@googlemail.com>
2018-09-17 14:31:54 +0100

    Removed unneeded files and merged index.js - markers now working again.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,49 @@ var express = require('express');
 var router = express.Router();
 var farmService = require("../services/farmService.js");
 
-/* GET home page. */
+router.get('/test', function(req, res, next) {
+	res.render('test', {
+		title: 'Express',
+		newkey: 'some text'
+	});
+});
+router.get('/farmlocations', function(req, res, next) {
+	res.send([
+		{
+			farmid: 1,
+			name: 'Farm 1',
+			lat: 56.4981776,
+			long: -3.0744827,
+			grown: true
+		},
+		{
+			farmid: 2,
+			name: 'Farm 2',
+			lat: 56.4970665,
+			long: -3.0744827,
+			grown: false
+		},
+		{
+			farmid: 3,
+			name: 'Farm 3',
+			lat: 56.4960554,
+			long: -3.0744827,
+			grown: false
+		}
+	]);
+});
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', {
+		title: 'Express'
+	});
+});
+
+router.get('/soil', function(req, res, next) {
+	res.render('soil', {
+		title: 'Express',
+		newkey: 'some text'
+	});
 });
 
 router.get('/getField', function (req,res,next) {
