2a0717b2184412395bf1f81c5591bd9fcbb33af9
Antony <acapper56@gmail.com>
2018-09-18 19:48:24 +0100

    Modified fakedata to create and insert weather information into the database

---
+++ app/respositories/databaseInsertFunctions.js
@@ -0,0 +1,28 @@
+module.exports = {
+	InsertWeather: function(dbc, data) {
+		return new Promise(function(resolve, reject) {
+			var query =
+				'call InsertWeather(' +
+				dbc.escape(data.date) +
+				',' +
+				dbc.escape(data.season) +
+				',' +
+				dbc.escape(data.temperature) +
+				',' +
+				dbc.escape(data.weather) +
+				',' +
+				dbc.escape(data.humidity) +
+				',' +
+				dbc.escape(data.windStrength) +
+				',' +
+				dbc.escape(data.id) +
+				',' +
+				dbc.escape(data.time) +
+				')';
+			console.log(query);
+			dbc.query(query, function(err) {
+				if (err) console.log(err);
+			});
+		});
+	}
+};
---
+++ app/services/fakeDataCreation.js
@@ -0,0 +1,145 @@
+var farmService = require('./farmService');
+var fakeDataService = require('./fakeDataService');
+
+var seasons = ['Spring', 'Summer', 'Autumn', 'Winter'];
+var weathers = [
+	'Cloudy',
+	'Rain',
+	'Sunny',
+	'Fog',
+	'Lighting',
+	'Snow',
+	'Windy',
+	'Clear'
+];
+
+function FakeData() {
+	this.ph;
+	this.moisture;
+	this.temp;
+	this.humid;
+	this.phAlpha;
+	this.moistureAlpha;
+	this.tempAlpha;
+	this.humidAlpha;
+	this.markers;
+
+	this.init = function() {
+		this.ph = Math.floor(Math.random() * 14);
+		this.moisture = Math.floor(Math.random() * 100);
+		this.temp = Math.floor(Math.random() * (45 - -15) + -15);
+		this.humid = Math.floor(Math.random() * 100);
+		this.wind = Math.floor(Math.random() * 150);
+		this.phAlpha = 0.05;
+		this.moistureAlpha = 0.1;
+		this.tempAlpha = 0.3;
+		this.humidAlpha = 0.2;
+		this.windAlpha = 0.5;
+
+		farmService.GetAllMarkers().then(function(data) {
+			markers = data.markers;
+			console.log(markers);
+		});
+	};
+
+	this.makeData = function() {
+		for (var i = 0; i < markers.length; i++) {
+			if (markers[i].Type == 'Field') {
+				this.makeFakeFieldData(markers[i]);
+			} else {
+				this.makeFakeWeatherData(markers[i]);
+			}
+		}
+	};
+
+	this.makeFakeWeatherData = function(marker) {
+		//WeatherID Auto
+		//RecordDate
+		var date = new Date();
+		//Season
+		var season = this.currentSeason(date);
+		//Temperature
+		var temperature = this.smoothRandom(this.tempAlpha, this.temp);
+		//Weathertype
+		var weather = weathers[Math.floor(Math.random() * weathers.length)];
+		//Humidity
+		var humidity = this.smoothRandom(this.humidAlpha, this.humid);
+		//WindStrength
+		var windStrength = this.smoothRandom(this.windAlpha, this.wind);
+		//FarmID
+		var id = marker.FarmID;
+		//CurrentTime
+		var time = date;
+
+		var data = {
+			date,
+			season,
+			temperature,
+			weather,
+			humidity,
+			windStrength,
+			id,
+			time
+		};
+
+		fakeDataService.InsertWeather(data);
+		console.log('Fake Weather Data');
+		console.log(data);
+	};
+
+	this.currentSeason = function(date) {
+		if (
+			date.getMonth() == 1 ||
+			date.getMonth() == 2 ||
+			date.getMonth() == 3
+		) {
+			return seasons[0];
+		} else if (
+			date.getMonth() == 4 ||
+			date.getMonth() == 5 ||
+			date.getMonth() == 6
+		) {
+			return seasons[1];
+		} else if (
+			date.getMonth() == 7 ||
+			date.getMonth() == 8 ||
+			date.getMonth() == 9
+		) {
+			return seasons[2];
+		} else {
+			return seasons[3];
+		}
+	};
+
+	this.makeFakeFieldData = function(marker) {
+		console.log('Fake Field Data');
+		//ID
+		//FarmFieldID
+		//PHLevel
+		//MoisturePercent
+		//CropID
+		//LocationID
+		//PlantDate
+		//RecordDate
+		//RecordTime
+		console.log(
+			'PH Level ' +
+				this.smoothRandom(this.phAlpha, this.ph) +
+				' ' +
+				'Moisture ' +
+				this.smoothRandom(this.moistureAlpha, this.moisture) +
+				'%'
+		);
+	};
+
+	this.smoothRandom = function(factor, start) {
+		if (start == 0) {
+			start = 1;
+		}
+		var max = start + start * factor;
+		var min = start - start * factor;
+		return Math.floor(Math.random() * (max - min) + min);
+	};
+}
+
+module.exports = FakeData;
