8a93eedfe7e13dfe6a3330f885bcdf7b1ef7940e
Antony <acapper56@gmail.com>
2018-09-18 16:55:34 +0100

    Removed references to user.js

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./app/routes/index');
-<<<<<<< HEAD
-var usersRouter = require('./app/routes/users');
 var databaseinsertsRouter = require('./app/routes/databaseinserts');
-=======
->>>>>>> DeanGlyn
 
 var app = express();
 
@@ -24,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/insert', databaseinsertsRouter);
 
 // catch 404 and forward to error handler
