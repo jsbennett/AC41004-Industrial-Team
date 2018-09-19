a9d2b6bb688dd3ce5fde7ce82125ef67ea59b183
jsbennett <bnnttjy@googlemail.com>
2018-09-19 19:38:28 +0100

    fixed errors

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var farmService = require('../services/farmService.js');
-var request = require('request');
+//var request = require('request');
 
 router.get('/field/:fieldID', function(req, res, next) {
 	farmService.GetCurrentFieldDetails(req, res).then(function(json) {
