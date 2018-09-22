fc8021124ff6b20d7f9a8eef996738af4655d414
Sam(sz) <szhay@dundee.ac.uk>
2018-09-22 08:33:16 +0100

    Styled summary tabs
    Still to fix date/time.
    Started analysis tab.

--- public/javascript/popup.js
+++ public/javascript/popup.js
@@ -17,7 +17,7 @@ function Crop(id) {
 		}
 	});
 }
-
+//
 function Soil() {
 	$.ajax({
 		url: '/soil/' + $('#id').data().bind,
@@ -36,3 +36,13 @@ function Summary() {
 		}
 	});
 }
+
+function Weather() {
+	$.ajax({
+		url: '/farm/1',
+		success: function(data) {
+			$('.leaflet-popup-content-wrapper').html(AddWrapper(data));
+		}
+	});
+}
+
