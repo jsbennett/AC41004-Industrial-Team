4af975b1fa784831e2e4b66af7d4b28fae021c2f
jsbennett <bnnttjy@googlemail.com>
2018-09-23 18:53:42 +0100

    updated pig

--- app/routes/index.js
+++ app/routes/index.js
@@ -28,13 +28,13 @@ router.get("/farmSummary/:farmID", function(req, res, next) {
 
 router.get("/weatherAnalysis/:farmID", function(req, res, next) {
     farmService.GetFarmAnalysis(req).then(function(json) {
-        res.render("analysis", { data: json });
+        res.render("weatherAnalysis", { data: json });
     });
 });
 
 router.get("/cropAnalysis/:farmID", function(req, res, next) {
     farmService.GetFarmAnalysis(req).then(function(json) {
-        res.render("analysis", { data: json });
+        res.render("cropAnalysis", { data: json });
     });
 });
 
