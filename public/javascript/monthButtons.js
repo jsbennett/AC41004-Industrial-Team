81f0bfb3ac190e8e60cd9e202d9dbc53d5df96fe
Antony Capper <acapper56@gmail.com>
2018-09-25 15:59:04 +0100

    Restyled analysis tabs

--- public/javascript/monthButtons.js
+++ public/javascript/monthButtons.js
@@ -1,6 +1,16 @@
 function displayAnalysis(month) {
-    for (var i = 0; i < 12; i++) {
-        $("#display" + i).hide();
-    }
-    $("#display" + month).show();
+	for (var i = 0; i < 12; i++) {
+		$('#display' + i).hide();
+		$('#' + i).removeClass('btn-primary');
+	}
+	$('#display' + month).show();
+	$('#' + month).addClass('btn-primary');
 }
+
+$(document).ready(function() {
+	var d = new Date();
+	var n = d.getMonth();
+	$('#display' + n).show();
+	$('#' + n).removeClass('btn-secondary');
+	$('#' + n).addClass('btn-primary');
+});
